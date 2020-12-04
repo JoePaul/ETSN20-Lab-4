ceb9e0e1f9d29a1830617b6fa3c256bcdddc6998
joepaul <jonathanpaul16@gmail.com>
2020-12-04 10:42:33 +0100

    Search application implemented

--- src/search/SearchApplication.java
+++ src/search/SearchApplication.java
@@ -2,6 +2,10 @@ package search;
 
 public class SearchApplication {
     public static void main(String[] args) {
-        Search.findPatternInFile(args[0], args[1]);
+        if (args[0].equalsIgnoreCase("search")) {
+            Search.findPatternInFile(args[0], args[1]);
+        } else {
+            System.out.println("Invalid command");
+        }
     }
 }
