5578f6093facc75e1f3845cd80fd827c693452d8
joepaul <jonathanpaul16@gmail.com>
2020-12-04 10:46:30 +0100

    Error with which argument is being passed to the function

--- src/search/SearchApplication.java
+++ src/search/SearchApplication.java
@@ -3,7 +3,7 @@ package search;
 public class SearchApplication {
     public static void main(String[] args) {
         if (args[0].equalsIgnoreCase("search")) {
-            Search.findPatternInFile(args[0], args[1]);
+            Search.findPatternInFile(args[1], args[2]);
         } else {
             System.out.println("Invalid command");
         }
